6a4a9bce5faf71310c0e7e9e31f5b4c1efa8d0f0
grace_goose <choieungi@gm.gist.ac.kr>
2023-03-17 00:51:07 +0900

    feat: parseStudentId and add loop termination condition

--- lib/utils/parser/grade/gradeReportParser.ts
+++ lib/utils/parser/grade/gradeReportParser.ts
@@ -1,28 +1,70 @@
 import path from "path";
-import {readFile} from "xlsx";
+import {readFile, WorkSheet} from "xlsx";
+import {start} from "repl";
 
-const TYPE_CELL_INDEX = 0;
-const CODE_CELL_INDEX = 1;
-const COURSE_NAME_CELL_INDEX = 3;
-const CREDIT_CELL_INDEX = 4;
-const GRADE_CELL_INDEX = 5;
+const TYPE_CELL_INDEX = 'A';
+const CODE_CELL_INDEX = 'B';
+const COURSE_NAME_CELL_INDEX = 'D';
+const CREDIT_CELL_INDEX = 'E';
+const GRADE_CELL_INDEX = 'F';
+const VALUE_KEY = 'v';
+const START_INDEX = 3;
 
 export class GradeReportParser {
 
   public static async readXlsxFile(file: any): Promise<void> {
-    let filePath = path.resolve(__dirname, 'grade_report.xls');
-    let workSheet = this.createSheet(filePath);
-    console.log(workSheet);
+    const filePath = path.resolve(__dirname, 'grade_report.xls');
+    const workSheet = SheetCreator.createSheet(filePath);
+    const studentId = this.parseStudentId(workSheet);
+
+    let index = START_INDEX;
+    while (true){
+      index++;
+
+      const workSheetElementElement = workSheet['D' + index.toString()]
+      if (workSheetElementElement !== undefined) console.log(workSheetElementElement);
+      if (this.notExistCodeRow(workSheet, index)){
+        continue;
+      }
+
+      if (this.isEndOfCode(workSheet, index)){
+        break;
+      }
+
+    }
+
   }
 
+  private static isEndOfCode(workSheet: WorkSheet, index: number){
+    const workSheetIdx = COURSE_NAME_CELL_INDEX + index.toString();
+    const workSheetElement:string = workSheet[workSheetIdx][VALUE_KEY];
+    return workSheetElement !== undefined && workSheetElement.includes('[학사]');
+  }
+
+  private static notExistCodeRow(workSheet: WorkSheet, index: number) {
+    const workSheetIdx = CODE_CELL_INDEX + index.toString();
+    const workSheetElement = workSheet[workSheetIdx];
+    return workSheetElement === undefined || workSheetIdx === 'Code';
+  }
+
+  private static parseStudentId(workSheet: any){
+    const studentIdString = workSheet['A2'][VALUE_KEY];
+    return studentIdString.split(':')[1].trim(); // format: StudentNo.:20205185
+  }
+
+}
+
+class SheetCreator{
+
   // TODO: change param type to FileWithPath
-  private static createSheet(file: any) {
+  public static createSheet(file: any) {
     const workBook = readFile(file);
     const sheetNames = workBook.SheetNames;
     if (sheetNames.length > 1) throw Error('유효하지 않은 파일입니다.');
     let sheetName = sheetNames[0];
     return workBook.Sheets[sheetName];
   }
+
 }
 
 GradeReportParser.readXlsxFile('');
