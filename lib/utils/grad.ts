6b65b886157849d096b8b2bf483b76f839fbae5f
eungi <egchoi@jobis.co>
2023-03-19 23:14:26 +0900

    feat: 파일 파싱 완료

--- lib/utils/grad.ts
+++ lib/utils/grad.ts
@@ -1,4 +1,5 @@
 import { GradStatusType } from '../types/grad';
+import { GradeReportParser } from './parser/grade/gradeReportParser';
 
 class HTTPError extends Error {
   constructor(messages?: string) {
@@ -7,12 +8,28 @@ class HTTPError extends Error {
   }
 }
 
+async function readFileAndParse(file: File) {
+  return new Promise(resolve => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const { result } = fileReader;
+      if (result) {
+        resolve(GradeReportParser.readXlsxFile(result as string));
+      }
+    };
+    fileReader.readAsBinaryString(file);
+  });
+}
+
 export default async function postGradStatusFile(
   gradeStatusFile: File,
   majorType: string
 ): Promise<GradStatusType> {
-  const BASE_URL = 'https://api.gijol.im';
+  const BASE_URL = 'https://dev-api.gijol.im';
   const payload = new FormData();
+
+  const gradeStatus = await readFileAndParse(gradeStatusFile);
+  console.log(gradeStatus);
 
   payload.append('majorType', majorType);
   payload.append('multipartFile', gradeStatusFile);
--- lib/utils/parser/grade/gradeReportParser.ts
+++ lib/utils/parser/grade/gradeReportParser.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
-import path from 'path';
-import {readFile, WorkSheet} from 'xlsx';
-import yargs from "yargs";
+import {read, WorkSheet} from 'xlsx';
+import {TakenCourse} from "./TakenCourse";
 
 const TYPE_CELL_INDEX = 'A';
 const CODE_CELL_INDEX = 'B';
@@ -12,45 +11,54 @@ const VALUE_KEY = 'v';
 const START_INDEX = 3;
 
 export class GradeReportParser {
-  public static async readXlsxFile(file: any): Promise<void> {
-    const filePath = path.resolve(__dirname, 'grade_report.xls');
+  public static readXlsxFile(filePath: string) {
     const workSheet = this.createSheet(filePath);
-    const studentId = this.parseStudentId(workSheet);
-
     let index = START_INDEX;
     const address = (cell: string, row: number) => cell + row;
+    let [year, semester] = ['', ''];
+    let takenCourses: TakenCourse[] = [];
 
-    while (index !== 100) {
+    while (true) {
       index += 1;
+      if (this.isEndOfCode(workSheet, address(CODE_CELL_INDEX, index))) {
+        break;
+      }
 
-      let year, semester;
       if (this.isSeparatedByYear(workSheet, address(COURSE_NAME_CELL_INDEX, index))) {
-        [year, semester] = this.parseYearAndSemester(workSheet, address(COURSE_NAME_CELL_INDEX, index));
+        [year, semester] = this.setYearAndSemester(workSheet, address(COURSE_NAME_CELL_INDEX, index));
       }
 
       if (this.notExistCodeRow(workSheet, address(CODE_CELL_INDEX, index))) {
         continue;
       }
 
-      if (this.isEndOfCode(workSheet, index)) {
-        break;
-      }
-
-
       const type = this.accessValueOfWorkSheet(workSheet, address(TYPE_CELL_INDEX, index))
       const code = this.accessValueOfWorkSheet(workSheet, address(CODE_CELL_INDEX, index))
       const course = this.accessValueOfWorkSheet(workSheet, address(COURSE_NAME_CELL_INDEX, index))
       const credit = this.accessValueOfWorkSheet(workSheet, address(CREDIT_CELL_INDEX, index))
       const grade = this.accessValueOfWorkSheet(workSheet, address(GRADE_CELL_INDEX, index));
-      console.log(type, code, course, credit, grade);
+      if (['F', 'U'].includes(grade)) {
+        continue;
+      }
 
+      const isLetterGrade: boolean = ['A', 'B', 'C', 'D'].some(letterGrade => grade.includes(letterGrade))
+      const canBeDuplicated = ['GS01', 'GS02', 'UC9331'].some(duplicatableCode => code.includes(duplicatableCode));
+      const addedTakenCourse = new TakenCourse(parseInt(year), semester, type, code, course, parseInt(credit), grade);
+
+      if (takenCourses.some(takenCourse => takenCourse.equals(addedTakenCourse) && isLetterGrade && !canBeDuplicated)) {
+        takenCourses = takenCourses.filter(course => !course.equals(addedTakenCourse))
+      }
+
+      takenCourses.push(addedTakenCourse);
     }
+
+    const studentId = this.parseStudentId(workSheet);
+    return {studentId: studentId, takenCourses};
   }
 
-  private static isEndOfCode(workSheet: WorkSheet, index: number) {
-    const workSheetIdx = CODE_CELL_INDEX + index.toString();
-    const workSheetElement: string = workSheet[workSheetIdx][VALUE_KEY];
-    return workSheetElement !== undefined && workSheetElement.includes('[학사]');
+  private static isEndOfCode(workSheet: WorkSheet, excelAddress: string) {
+    const workSheetElement: string = this.accessValueOfWorkSheet(workSheet, excelAddress)
+    return workSheetElement.includes('[학사]');
   }
 
   private static notExistCodeRow(workSheet: WorkSheet, excelAddress: string) {
@@ -63,7 +71,7 @@ export class GradeReportParser {
     return workSheetElement && workSheetElement.includes('학기>');
   }
 
-  private static parseYearAndSemester(workSheet: WorkSheet, excelAddress: string) {
+  private static setYearAndSemester(workSheet: WorkSheet, excelAddress: string) {
     const workSheetElement = this.accessValueOfWorkSheet(workSheet, excelAddress);
     const elementWithNoWhitespace = workSheetElement.trim();
     const [year, semester] = elementWithNoWhitespace.substring(1, elementWithNoWhitespace.length - 1).split('/');
@@ -74,7 +82,7 @@ export class GradeReportParser {
     if (excelAddress in workSheet) {
       const workSheetElement = workSheet[excelAddress];
       if (VALUE_KEY in workSheetElement) {
-        return workSheetElement[VALUE_KEY];
+        return workSheetElement[VALUE_KEY].trim();
       }
     }
 
@@ -86,13 +94,11 @@ export class GradeReportParser {
     return studentIdString.split(':')[1].trim(); // format: StudentNo.:20205185
   }
 
-  private static createSheet(file: any) {
-    const workBook = readFile(file);
+  private static createSheet(file: string) {
+    const workBook = read(file, {type: "binary"});
     const sheetNames = workBook.SheetNames;
     if (sheetNames.length > 1) throw Error('유효하지 않은 파일입니다.');
     const sheetName = sheetNames[0];
     return workBook.Sheets[sheetName];
   }
 }
-
-GradeReportParser.readXlsxFile('');
