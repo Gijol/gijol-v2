5a29c08ba93f73e0b41074704afb697520ba042f
eungi <egchoi@jobis.co>
2023-03-19 20:08:23 +0900

    feat: 전체 강의 파싱 추가

--- lib/utils/parser/grade/gradeReportParser.ts
+++ lib/utils/parser/grade/gradeReportParser.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import {readFile, WorkSheet} from "xlsx";
-import {start} from "repl";
+import {add} from "unload";
 
 const TYPE_CELL_INDEX = 'A';
 const CODE_CELL_INDEX = 'B';
@@ -12,58 +12,77 @@ const START_INDEX = 3;
 
 export class GradeReportParser {
 
-  public static async readXlsxFile(file: any): Promise<void> {
-    const filePath = path.resolve(__dirname, 'grade_report.xls');
-    const workSheet = SheetCreator.createSheet(filePath);
-    const studentId = this.parseStudentId(workSheet);
+    public static async readXlsxFile(file: any): Promise<void> {
+        const filePath = path.resolve(__dirname, 'grade_report.xls');
+        const workSheet = SheetCreator.createSheet(filePath);
+        const studentId = this.parseStudentId(workSheet);
 
-    let index = START_INDEX;
-    while (true){
-      index++;
+        let index = START_INDEX;
+        const address = (cell: string, index: number) => cell + index;
 
-      const workSheetElementElement = workSheet['D' + index.toString()]
-      if (workSheetElementElement !== undefined) console.log(workSheetElementElement);
-      if (this.notExistCodeRow(workSheet, index)){
-        continue;
-      }
+        while (index !== 100) {
+            index++;
 
-      if (this.isEndOfCode(workSheet, index)){
-        break;
-      }
+            const type = this.accessValueOfWorkSheet(workSheet, address(TYPE_CELL_INDEX, index));
+            const code = this.accessValueOfWorkSheet(workSheet, address(CODE_CELL_INDEX, index));
+            const course = this.accessValueOfWorkSheet(workSheet, address(COURSE_NAME_CELL_INDEX, index));
+            const credit = this.accessValueOfWorkSheet(workSheet, address(CREDIT_CELL_INDEX, index));
+            const grade = this.accessValueOfWorkSheet(workSheet, address(GRADE_CELL_INDEX, index));
+            // console.log(type, code, course, credit, grade);
+
+
+            if (this.notExistCodeRow(workSheet, index)) {
+                continue;
+            }
+
+            if (this.isEndOfCode(workSheet, index)) {
+                break;
+            }
+
+        }
 
     }
 
-  }
+    private static isEndOfCode(workSheet: WorkSheet, index: number) {
+        const workSheetIdx = CODE_CELL_INDEX + index.toString();
+        const workSheetElement: string = workSheet[workSheetIdx][VALUE_KEY];
+        return workSheetElement !== undefined && workSheetElement.includes('[학사]');
+    }
 
-  private static isEndOfCode(workSheet: WorkSheet, index: number){
-    const workSheetIdx = COURSE_NAME_CELL_INDEX + index.toString();
-    const workSheetElement:string = workSheet[workSheetIdx][VALUE_KEY];
-    return workSheetElement !== undefined && workSheetElement.includes('[학사]');
-  }
+    private static notExistCodeRow(workSheet: WorkSheet, index: number) {
+        const workSheetIdx = CODE_CELL_INDEX + index.toString();
+        const workSheetElement = workSheet[workSheetIdx];
+        return workSheetElement === undefined || workSheetIdx === 'Code';
+    }
 
-  private static notExistCodeRow(workSheet: WorkSheet, index: number) {
-    const workSheetIdx = CODE_CELL_INDEX + index.toString();
-    const workSheetElement = workSheet[workSheetIdx];
-    return workSheetElement === undefined || workSheetIdx === 'Code';
-  }
+    private static accessValueOfWorkSheet(workSheet: WorkSheet, excelAddress: string): string {
+        if (excelAddress in workSheet) {
+            const workSheetElement = workSheet[excelAddress]
+            if (VALUE_KEY in workSheetElement) {
+                return workSheetElement[VALUE_KEY];
+            }
+        }
 
-  private static parseStudentId(workSheet: any){
-    const studentIdString = workSheet['A2'][VALUE_KEY];
-    return studentIdString.split(':')[1].trim(); // format: StudentNo.:20205185
-  }
+        return '';
+    }
+
+    private static parseStudentId(workSheet: any) {
+        const studentIdString = workSheet['A2'][VALUE_KEY];
+        return studentIdString.split(':')[1].trim(); // format: StudentNo.:20205185
+    }
 
 }
 
-class SheetCreator{
+class SheetCreator {
 
-  // TODO: change param type to FileWithPath
-  public static createSheet(file: any) {
-    const workBook = readFile(file);
-    const sheetNames = workBook.SheetNames;
-    if (sheetNames.length > 1) throw Error('유효하지 않은 파일입니다.');
-    let sheetName = sheetNames[0];
-    return workBook.Sheets[sheetName];
-  }
+    // TODO: change param type to FileWithPath
+    public static createSheet(file: any) {
+        const workBook = readFile(file);
+        const sheetNames = workBook.SheetNames;
+        if (sheetNames.length > 1) throw Error('유효하지 않은 파일입니다.');
+        let sheetName = sheetNames[0];
+        return workBook.Sheets[sheetName];
+    }
 
 }
 
