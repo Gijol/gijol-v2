5967535af2255545ca56e56f16b52fc20768b654
Dongho Seo <ehcws333@gmail.com>
2024-01-12 22:26:15 +0900

    fix: duplicated file on navlink issue

--- components/layouts/layout-navbar.tsx
+++ components/layouts/layout-navbar.tsx
@@ -1,33 +1,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-<<<<<<< HEAD
 
 import { createStyles, Navbar, NavLink, rem, Drawer } from '@mantine/core';
 import { getCntTab } from '@utils/status';
 import { navLinks } from '@const/nav-links';
-=======
-import { navLinks } from '@const/nav-links';
-import { getCntTab } from '@utils/status';
-
-const useStyles = createStyles((theme) => ({
-  navbar: {
-    paddingTop: 0,
-    wordBreak: 'keep-all',
-  },
-
-  section: {
-    marginLeft: `calc(${theme.spacing.md} * -1)`,
-    marginRight: `calc(${theme.spacing.md} * -1)`,
-    marginBottom: theme.spacing.md,
-
-    '&:not(:last-of-type)': {
-      borderBottom: `${rem(1)} solid ${
-        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-      }`,
-    },
-  },
-}));
->>>>>>> 223d28d50f2383c55814d72fdf84a84657d75ad3
 
 export function LayoutNavbar({
   matches,
