b3c3eef6a6cf847e499cde824e1c8e245c37096c
Dongho Seo <88386139+sleeep23@users.noreply.github.com>
2024-01-14 00:42:00 +0900

    Repo migration workflow - deployment error fixed (#37)
    * gijol-v2.0.1  (#35)
    
    * feat: add form submit section at grad certificate builder
    
    * chore: fix form value namings
    
    * ui: certificate page with mutiple inputs in grid
    
    * ui: fix overall and specific graduation info design
    
    * ui: fix grad status page design
    
    * feat: change input with debounce auto search using react-hook-form & react-query
    
    * ui: improve my course info design
    
    * ui: fix grad status and recommend page ui + chore: add small ex of certificate parser
    
    * feat: add certificate excel upload and parse & init with xlsx & rhf
    
    * fix: remove deprecated and unused file
    
    * chore: set path alias
    
    * chore: organzie import declaration
    
    * ui: add a dropzone for certificate file upload and initialize form with parsed info
    
    * feat: add readonly number inputs for certificate builder & ui change bit
    
    * chore: update fetching logic and add loading overlay
    
    * feat: add timetable generator test ui
    
    * chore: add reset button for options
    
    * chore: fix loading skeleton
    
    * chore: add event add btn at timetable
    
    * Update ui changes (#34)
    
    * feat: add form submit section at grad certificate builder
    
    * chore: fix form value namings
    
    * ui: certificate page with mutiple inputs in grid
    
    * ui: fix overall and specific graduation info design
    
    * ui: fix grad status page design
    
    * feat: change input with debounce auto search using react-hook-form & react-query
    
    * ui: improve my course info design
    
    * ui: fix grad status and recommend page ui + chore: add small ex of certificate parser
    
    * feat: add certificate excel upload and parse & init with xlsx & rhf
    
    * fix: remove deprecated and unused file
    
    * chore: set path alias
    
    * chore: organzie import declaration
    
    * ui: add a dropzone for certificate file upload and initialize form with parsed info
    
    * feat: add readonly number inputs for certificate builder & ui change bit
    
    * chore: update fetching logic and add loading overlay
    
    * feat: add timetable generator test ui
    
    * chore: add reset button for options
    
    * chore: fix loading skeleton
    
    * chore: add event add btn at timetable
    
    * chore: add ui changes on course/my page and add update label on navlinks, preparing production merge
    
    * fix: fix package.json issues
    
    * fix: duplicated file on navlink issue
    
    ---------
    
    Co-authored-by: grace_goose <choieungi@gm.gist.ac.kr>
    Co-authored-by: eungi <egchoi@jobis.co>
    Co-authored-by: codesee-maps[bot] <86324825+codesee-maps[bot]@users.noreply.github.com>
    
    * feat: add workflow to push contents to gijol-official repository
    
    * Revert "gijol-v2.0.1  (#35)"
    
    This reverts commit cd82ded7a40008a063b583580c38b070f6a34ced.
    
    * Revert "[Fix] UI improvement, Feedback applied (#32)"
    
    This reverts commit c6c5f6a9105bc80b281eb2b3f76b37745adda678.
    
    * chore: deployment error resolved
    
    ---------
    
    Co-authored-by: grace_goose <choieungi@gm.gist.ac.kr>
    Co-authored-by: eungi <egchoi@jobis.co>
    Co-authored-by: codesee-maps[bot] <86324825+codesee-maps[bot]@users.noreply.github.com>

--- components/course-my-grade-chart.tsx
+++ components/course-my-grade-chart.tsx
@@ -1,5 +1,4 @@
 import {
-  CartesianAxis,
   CartesianGrid,
   LabelList,
   Line,
@@ -11,11 +10,9 @@ import {
 } from 'recharts';
 import React from 'react';
 
-import { Notification, Paper, Title, useMantineTheme } from '@mantine/core';
-import React from 'react';
-import { CourseListWithPeriod } from '../lib/utils/status';
+import { Paper, Title, useMantineTheme } from '@mantine/core';
+import { CourseListWithPeriod } from '@utils/status';
 import CustomTooltip from '@components/rechart-custom-tooltip';
-
 
 const generateLineChartData = (courseListWithPeriod: CourseListWithPeriod[]) => {
   return courseListWithPeriod.map((periodWithList: any) => {
