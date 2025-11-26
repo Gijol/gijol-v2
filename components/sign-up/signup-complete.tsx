7f1981bf545fbaffb695249a9a95c14302228bab
Dongho Seo <ehcws333@gmail.com>
2025-11-27 02:45:59 +0900

    fix: lottie ssr error fixed

--- components/sign-up/signup-complete.tsx
+++ components/sign-up/signup-complete.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import Lottie from 'react-lottie-player';
-import ThumbsUp from '../../public/lottie/thumbs-up.json';
 import { Button, Container, Space, Text } from '@mantine/core';
-import { FileWithPath } from '@mantine/dropzone';
 import { useRouter } from 'next/router';
-
-class HTTPError extends Error {
-  constructor(messages?: string) {
-    super(messages);
-    this.name = 'HTTP Error';
-  }
-}
 
 export default function SignupComplete() {
   const router = useRouter();
@@ -23,7 +13,6 @@ export default function SignupComplete() {
         padding: 40,
       }}
     >
-      <Lottie animationData={ThumbsUp} play style={{ width: '150', height: '150' }} loop={false} />
       <Space h={16} />
       <Text align="center" size="xl" weight={500}>
         ✨ 이제 회원가입이 끝났습니다! 그럼 확인하러 가볼까요? ✨
